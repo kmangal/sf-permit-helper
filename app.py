7b7c5aa3bdc147d2556ea91e81e0af1e7ebcb0ca
Kunal Mangal <kmangal0991@gmail.com>
2026-09-12 13:32:27 -0700

    permit types

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 
+from permits import PERMITS
+
 app = FastAPI()
 
 app.add_middleware(
@@ -16,19 +18,29 @@ class PermitQuery(BaseModel):
     description: str
 
 
+@app.get("/api/permit-types")
+async def list_permit_types():
+    return [
+        {
+            "id": pt.value,
+            "name": info.name,
+            "description": info.description,
+            "url": info.url,
+        }
+        for pt, info in PERMITS.items()
+    ]
+
+
 @app.post("/api/permits")
 async def find_permits(query: PermitQuery):
-    # TODO: replace with real permit lookup logic
+    # TODO: replace with real permit lookup logic (RAG)
     permits = [
         {
-            "name": "Building Permit",
-            "description": "Required for structural changes to a building.",
-            "link": "https://sf.gov/topics/building-permits",
-        },
-        {
-            "name": "Electrical Permit",
-            "description": "Required for electrical work.",
-            "link": "https://sf.gov/topics/building-permits",
-        },
+            "id": pt.value,
+            "name": info.name,
+            "description": info.description,
+            "url": info.url,
+        }
+        for pt, info in PERMITS.items()
     ]
-    return {"permits": permits}
+    return {"permits": permits[:3]}
